74b31783f0548030cff83bc16d4d42b37124fb81
suraj9032 <suryaprakashpatidar0@gmail.com>
2024-11-22 12:23:28 +0530

    company_size_get

--- application/models/company_size_model.php
+++ application/models/company_size_model.php
@@ -1,22 +1,27 @@
 <?php
 class Company_size_model extends CI_Model {
 
-public function __construct() {
-    parent::__construct();
-}
-
-// Fetch all company sizes
-public function company_sizes() {
-    // Fetch data from the company_size table
-    $this->db->select('id, size_name');
-    $this->db->from('company_size');
-    $query = $this->db->get();
-
-    // Return the result as an associative array
-    if ($query->num_rows() > 0) {
-        return $query->result_array(); 
-    } else {
-        return false; // No data found
+    public function __construct() {
+        parent::__construct();
     }
-}
+
+    // Fetch distinct company sizes from the user table
+    public function company_sizes() {
+        $this->db->distinct();
+        $this->db->select('company_size_id, company_size_name'); // Adjust fields as necessary
+        $this->db->from('users'); // Use the user table
+        $query = $this->db->get();
+
+        if ($query === false) {
+            log_message('error', 'Database Error: ' . $this->db->error()['message']);
+            log_message('error', 'Last Query: ' . $this->db->last_query());
+            return false;
+        }
+
+        if ($query->num_rows() > 0) {
+            return $query->result_array();
+        } else {
+            return []; // Return an empty array if no data found
+        }
+    }
 }
