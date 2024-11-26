cf175464c4d11dff2bc127de64734434c86b7a57
Balram Patidar <bilspatidar@gmail.com>
2024-11-26 15:32:07 +0000

    csize

--- application/models/Company_size_model.php
+++ application/models/Company_size_model.php
@@ -1,27 +1,20 @@
 <?php
 class Company_size_model extends CI_Model {
+     protected $table      = 'company_size';
+     protected $primaryKey = 'id';
 
     public function __construct() {
         parent::__construct();
+
     }
 
     // Fetch distinct company sizes from the user table
     public function company_sizes() {
-        $this->db->distinct();
-        $this->db->select('company_size_id, company_size_name'); // Adjust fields as necessary
-        $this->db->from('users'); // Use the user table
-        $query = $this->db->get();
-
-        if ($query === false) {
-            log_message('error', 'Database Error: ' . $this->db->error()['message']);
-            log_message('error', 'Last Query: ' . $this->db->last_query());
-            return false;
+        $this->db->select("*");
+        $this->db->from($this->table);
+        if(!empty($id)) {
+            $this->db->where($this->primaryKey, $id);
         }
-
-        if ($query->num_rows() > 0) {
-            return $query->result_array();
-        } else {
-            return []; // Return an empty array if no data found
-        }
+        return $this->db->get()->result();
     }
 }
